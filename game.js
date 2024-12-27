90d643b60b1a5c6bc0f81f343a96ecb3b808dbd1
zlyxj <xiaomei8229@sina.com>
2024-12-27 21:38:57 +0800

    Update game.js

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ function getpoints()
 {
    if(localStorage.x){
    localStorage.x=new D(localStorage.x).times(new D(localStorage.x))
+   localStorage.x=new D.fromComponents((new D(localStorage.x)).sign,(new D(localStorage.x)).layer,Math.round(new D(localStorage.x).mag*1000)/1000)
   document.getElementById("pts").innerHTML=localStorage.x
 }
 else{
